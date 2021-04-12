43f5e1cb96b0d8473238716515635d6b4f393bda
bdourdoufis <dourdoufis.br@husky.neu.edu>
2021-04-12 00:11:36 -0400

    assignment 8 - Heroku Deployment

--- src/services/questions-service.js
+++ src/services/questions-service.js
@@ -1,4 +1,8 @@
+import {} from 'dotenv/config'
+
 const QUIZZES_URL = process.env.QUIZ_SERVICE_URI || 'http://localhost:4000/api/quizzes';
+
+
 const findQuestionsForQuiz = (qid) => {
     return fetch(`${QUIZZES_URL}/${qid}/questions`)
         .then(response => response.json())
--- src/services/quizzes-service.js
+++ src/services/quizzes-service.js
@@ -1,4 +1,7 @@
+import {} from 'dotenv/config'
+
 const QUIZZES_URL = process.env.QUIZ_SERVICE_URI || 'http://localhost:4000/api/quizzes';
+
 const findAllQuizzes = () => {
     return fetch(QUIZZES_URL)
         .then(response => response.json())
